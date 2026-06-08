34d349958823cda450afc2a913cda11b22fbe497
vinicius1978pimenta <144499204+vinicius1978pimenta@users.noreply.github.com>
2026-06-08 18:21:49 -0300

    fix-pedro-merge

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -3,17 +3,21 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { PrismaModule } from '@/prisma/prisma.module';
 import { PermissionsGuard } from './infrastructure/guards/permissions.guard';
 import { RolesInterceptor } from './infrastructure/interceptors/roles.interceptor';
+import { AuthController } from './presentation/controllers/auth.controller';
+import { AuthService } from './application/services/auth.service';
 
 @Global()
 @Module({
   imports: [PrismaModule],
+  controllers: [AuthController],
   providers: [
+    AuthService,
     PermissionsGuard,
     {
       provide: APP_INTERCEPTOR,
       useClass: RolesInterceptor,
     },
   ],
-  exports: [PermissionsGuard],
+  exports: [PermissionsGuard, AuthService],
 })
 export class AuthModule {}
