aa881eb176d77b5c843583054a508e3b9b327c1b
Ocirne <153968073+enriconovellino@users.noreply.github.com>
2026-06-19 14:15:48 -0300

    fix/adicionando atributos a entidade atividade

--- backend/src/modules/atividades/entities/atividade.entity.ts
+++ backend/src/modules/atividades/entities/atividade.entity.ts
@@ -5,11 +5,17 @@ export class Atividade {
     titulo_atividade!: string;
     descricao_atividade!: string;
     tipo_atividade!: string;
+    pergunta!: string;
+    resposta_correta!: string;
+    licao_id!: number;
     
-    constructor(id: number, titulo_atividade: string, descricao_atividade: string, tipo_atividade: string) {
+    constructor(id: number, titulo_atividade: string, descricao_atividade: string, tipo_atividade: string, pergunta: string, resposta_correta: string, licao_id: number) {
         this.id = id;
-        this.tipo_atividade = titulo_atividade;
+        this.titulo_atividade = titulo_atividade;
         this.descricao_atividade = descricao_atividade;
         this.tipo_atividade = tipo_atividade;
+        this.pergunta = pergunta;
+        this.resposta_correta = resposta_correta;
+        this.licao_id = licao_id;
     }
 }
