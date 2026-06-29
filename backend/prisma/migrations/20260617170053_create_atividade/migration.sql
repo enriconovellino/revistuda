4acba87e9eadce221b98e166cbb705ceab7445f0
Ocirne <153968073+enriconovellino@users.noreply.github.com>
2026-06-29 17:05:23 -0300

    fix/migration da atividade

--- backend/prisma/migrations/20260617170053_create_atividade/migration.sql
+++ backend/prisma/migrations/20260617170053_create_atividade/migration.sql
@@ -4,6 +4,7 @@ CREATE TABLE "Atividade" (
     "titulo_atividade" TEXT NOT NULL,
     "descricao_atividade" TEXT,
     "tipo_atividade" TEXT NOT NULL,
+    "licao_id" INTEGER NOT NULL,
 
     CONSTRAINT "Atividade_pkey" PRIMARY KEY ("atividade_id")
 );
