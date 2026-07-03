f748d181498a2dcde875d87af5643af0201fc320
Ocirne <153968073+enriconovellino@users.noreply.github.com>
2026-07-03 17:22:59 -0300

    fix/subindo tres aquivos

---
+++ backend/src/modules/turmas/domain/use-cases/get-turmas-professor.use-case.ts
@@ -0,0 +1,13 @@
+import { Inject, Injectable } from '@nestjs/common';
+import type { ITurmaRepository } from '../ports/turma-repository.port';
+import { TURMA_REPOSITORY } from '../ports/turma-repository.port';
+import { Turma } from '../entities/turma.entity';
+
+@Injectable()
+export class GetTurmasProfessorUseCase {
+  constructor(@Inject(TURMA_REPOSITORY) private turmaRepository: ITurmaRepository) {}
+
+  async execute(professorId: number): Promise<Turma[]> {
+    return this.turmaRepository.findByProfessorId(professorId);
+  }
+}
