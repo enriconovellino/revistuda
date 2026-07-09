6ec1e51edf6563e1e264644446351ec48dd1af1d
felipemarroni <fmarronem@gmail.com>
2026-07-09 14:08:59 -0300

    todas as rotas de saída do sistema agora apontam para a landing page

--- frontend/src/app/pages/adm/adm.component.ts
+++ frontend/src/app/pages/adm/adm.component.ts
@@ -200,6 +200,6 @@ export class AdmComponent implements OnInit {
     if (typeof window !== 'undefined' && window.localStorage) {
       localStorage.clear();
     }
-    this.router.navigate(['/auth']);
+    this.router.navigate(['/']);
   }
 }
--- frontend/src/app/pages/professor/modulo-detalhe/modulo-detalhe.component.ts
+++ frontend/src/app/pages/professor/modulo-detalhe/modulo-detalhe.component.ts
@@ -632,6 +632,6 @@ export class ModuloDetalheComponent implements OnInit {
     if (typeof window !== 'undefined' && window.localStorage) {
       localStorage.clear();
     }
-    this.router.navigate(['/auth']);
+    this.router.navigate(['/']);
   }
 }
