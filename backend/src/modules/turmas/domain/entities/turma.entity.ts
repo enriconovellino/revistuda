dba3f2ec99dc4181ac7c8a7b4b566676f12bf449
felipemarroni <fmarronem@gmail.com>
2026-07-11 14:51:14 -0300

    fazendo a alteração de totalAlunos de novo

--- backend/src/modules/turmas/domain/entities/turma.entity.ts
+++ backend/src/modules/turmas/domain/entities/turma.entity.ts
@@ -4,7 +4,7 @@ export class Turma {
   descricao_turma?: string;
   capacidade_maxima?: number;
   professor_id?: number | null;
-  total_alunos?: number;
+  totalAlunosalunos?: number;
 
   constructor(
     turma_id: number,
@@ -12,13 +12,13 @@ export class Turma {
     professor_id?: number | null,
     descricao_turma?: string,
     capacidade_maxima?: number,
-    total_alunos?: number,
+    totalAlunosalunos?: number,
   ) {
     this.turma_id = turma_id;
     this.nome_turma = nome_turma;
     this.professor_id = professor_id;
     this.descricao_turma = descricao_turma;
     this.capacidade_maxima = capacidade_maxima;
-    this.total_alunos = total_alunos;
+    this.totalAlunosalunos = totalAlunosalunos;
   }
 }
