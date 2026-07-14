1f9a96ca9f70541b81089fb902958929bd870504
felipemarroni <fmarronem@gmail.com>
2026-07-14 14:46:06 -0300

    consertando camiho do auth.service

--- frontend/src/app/pages/aluno-idoso/sidebar/sidebar.component.ts
+++ frontend/src/app/pages/aluno-idoso/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-sidebar-idoso',
